96dc0093c539c268123efd4f79f60fb907c4b9ab
p.dikin <dikinp@mail.ru>
2020-06-25 07:49:34 +0300

    init commit

---
+++ src/main/java/myProject/Application.java
@@ -0,0 +1,16 @@
+package myProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author paveldikin
+ * @date 25.06.2020
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
